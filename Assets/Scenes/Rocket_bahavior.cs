637fe1c0e33665919ef9285ae9bc5fcc584d9973
Adam Hostin <xhosti02@vutbr.cz>
2019-08-24 21:27:12 +0200

    Rocket finished Basic Key input handling rigid body prepared for using

---
+++ Assets/Scenes/Rocket_bahavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket_bahavior : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHandler();
+    }
+
+    private void InputHandler()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) print("up");
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) print("rotate right");
+
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) print("rotate Left");
+
+
+
+    }
+}
