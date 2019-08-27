5d1cde58103e419611fe3ca35c8c2ad6ef2c846b
Adam Hostin <xhosti02@vutbr.cz>
2019-08-27 18:43:34 +0200

    audio

--- Assets/Scenes/Rocket_bahavior.cs
+++ Assets/Scenes/Rocket_bahavior.cs
@@ -7,11 +7,13 @@ public class Rocket_bahavior : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,7 +25,15 @@ public class Rocket_bahavior : MonoBehaviour
     private void InputHandler()
     {
         const int rotation_value = 25;
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) rigidbody.AddRelativeForce(Vector3.up);
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            if (!audioSource.isPlaying) audioSource.Play();
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        else
+        {
+            if (audioSource.isPlaying) audioSource.Stop();
+        }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) transform.Rotate(Vector3.back, rotation_value * Time.deltaTime);
 
