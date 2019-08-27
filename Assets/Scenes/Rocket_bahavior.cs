0870cc94ab613285464ba8d7959a3279105c4d62
Adam Hostin <xhosti02@vutbr.cz>
2019-08-28 00:14:16 +0200

    tags movement improvment first level

--- Assets/Scenes/Rocket_bahavior.cs
+++ Assets/Scenes/Rocket_bahavior.cs
@@ -8,6 +8,10 @@ public class Rocket_bahavior : MonoBehaviour
 
     Rigidbody rigidbody;
     AudioSource audioSource;
+    [SerializeField] float rotation_value = 80f;
+    [SerializeField] float thrust_value = 80f;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,27 +23,37 @@ public class Rocket_bahavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputHandler();
-    }
-
-    private void InputHandler()
-    {
-        const int rotation_value = 25;
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
             if (!audioSource.isPlaying) audioSource.Play();
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * thrust_value);
         }
         else
         {
             if (audioSource.isPlaying) audioSource.Stop();
         }
 
+        rigidbody.freezeRotation = true;
+
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) transform.Rotate(Vector3.back, rotation_value * Time.deltaTime);
 
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(Vector3.forward, rotation_value * Time.deltaTime);
 
-
-
+        rigidbody.freezeRotation = false;
     }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                break;
+            default:
+                print("you died");
+                break;
+        }
+    }
+
 }
