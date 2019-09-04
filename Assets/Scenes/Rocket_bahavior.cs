d295d822e140ce8bb6334f78b4eea591e4162de0
Adam Hostin <xhosti02@vutbr.cz>
2019-09-05 00:44:28 +0200

    invoke added preperation for playing win and lose sounds

--- Assets/Scenes/Rocket_bahavior.cs
+++ Assets/Scenes/Rocket_bahavior.cs
@@ -1,5 +1,6 @@
 ﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket_bahavior : MonoBehaviour
 {
@@ -7,6 +8,11 @@ public class Rocket_bahavior : MonoBehaviour
     AudioSource audioSource;
     [SerializeField] float rotation_value = 80f;
     [SerializeField] float thrust_value = 80f;
+    [SerializeField] float delay = 1f;
+    int level_index;
+    const int max_level = 2;
+    enum state {Alive,Dead,Half_Dead};
+    state current_state;
 
 
 
@@ -15,11 +21,16 @@ public class Rocket_bahavior : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        level_index = 0;
+        current_state = state.Alive;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (current_state == state.Dead) return;
+        
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
             if (!audioSource.isPlaying) audioSource.Play();
@@ -37,20 +48,37 @@ public class Rocket_bahavior : MonoBehaviour
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(Vector3.forward, rotation_value * Time.deltaTime);
 
         rigidbody.freezeRotation = false;
+        
     }
 
     void OnCollisionEnter (Collision collision)
     {
+        if (current_state == state.Dead) return;
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 break;
             case "Finish":
+
+                if (max_level==level_index) Invoke("LoadScene", delay);
+                else {
+                    level_index++;
+                    Invoke("LoadScene", delay);
+                }
                 break;
             default:
                 print("you died");
+                current_state = state.Dead;
+                level_index = 0;
+                Invoke("LoadScene", delay);
+                
                 break;
         }
+    }
+    void LoadScene()
+    {
+        SceneManager.LoadScene(level_index);
+        current_state = state.Alive;
     }
 
 }
