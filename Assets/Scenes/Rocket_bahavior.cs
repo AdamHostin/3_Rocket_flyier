3840ee0dc2975690b7ccab793cfd02b1ed641699
Adam Hostin <xhosti02@vutbr.cz>
2019-09-06 22:00:35 +0200

    new sounds

--- Assets/Scenes/Rocket_bahavior.cs
+++ Assets/Scenes/Rocket_bahavior.cs
@@ -1,4 +1,6 @@
-﻿
+﻿//https://freesound.org/people/jacksonacademyashmore/sounds/414209/ death sound
+//https://freesound.org/people/FunWithSound/sounds/456966/ success fanfare
+//https://freesound.org/people/GabrielAraujo/sounds/242501/ success point given
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,11 +10,19 @@ public class Rocket_bahavior : MonoBehaviour
     AudioSource audioSource;
     [SerializeField] float rotation_value = 80f;
     [SerializeField] float thrust_value = 80f;
+    [SerializeField] float thrust_sound_value = 0.75f;
     [SerializeField] float delay = 1f;
-    int level_index;
+
+    [SerializeField] AudioClip Thrust_sound;
+    [SerializeField] AudioClip Game_win_sound;
+    [SerializeField] AudioClip Level_win_sound;
+    [SerializeField] AudioClip Death_sound;
+
+        int level_index;
     const int max_level = 2;
-    enum state {Alive,Dead,Half_Dead};
+    enum state {Alive,Dead,ChangingLevel};
     state current_state;
+
 
 
 
@@ -29,18 +39,28 @@ public class Rocket_bahavior : MonoBehaviour
     void Update()
     {
 
-        if (current_state == state.Dead) return;
-        
+        if (current_state != state.Alive) return;
+
+        ThrustHandling();
+        RotationHandling();
+
+    }
+
+    private void ThrustHandling()
+    {
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
-            if (!audioSource.isPlaying) audioSource.Play();
+            if (!audioSource.isPlaying) audioSource.PlayOneShot(Thrust_sound, thrust_sound_value);
             rigidbody.AddRelativeForce(Vector3.up * thrust_value);
         }
         else
         {
             if (audioSource.isPlaying) audioSource.Stop();
         }
+    }
 
+    private void RotationHandling()
+    {
         rigidbody.freezeRotation = true;
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) transform.Rotate(Vector3.back, rotation_value * Time.deltaTime);
@@ -48,21 +68,26 @@ public class Rocket_bahavior : MonoBehaviour
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(Vector3.forward, rotation_value * Time.deltaTime);
 
         rigidbody.freezeRotation = false;
-        
     }
 
     void OnCollisionEnter (Collision collision)
     {
-        if (current_state == state.Dead) return;
+        if (current_state != state.Alive) return;
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 break;
             case "Finish":
-
-                if (max_level==level_index) Invoke("LoadScene", delay);
-                else {
+                current_state = state.ChangingLevel;                
+                if (max_level == level_index)
+                {
+                    audioSource.PlayOneShot(Game_win_sound);
+                    Invoke("LoadScene", delay);
+                }
+                else
+                {
                     level_index++;
+                    audioSource.PlayOneShot(Level_win_sound);
                     Invoke("LoadScene", delay);
                 }
                 break;
@@ -70,8 +95,10 @@ public class Rocket_bahavior : MonoBehaviour
                 print("you died");
                 current_state = state.Dead;
                 level_index = 0;
+                if (audioSource.isPlaying) audioSource.Stop();
+                audioSource.PlayOneShot(Death_sound);
                 Invoke("LoadScene", delay);
-                
+
                 break;
         }
     }
